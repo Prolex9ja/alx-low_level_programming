a7db5944eeaf103f906e621b5c5ca2ee71517222
lex9ja <alexanderedim8@yahoo.com>
2022-09-14 01:08:47 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98
+ * print_to_98 - prints all natural numbers from n to 98,
  * followed by a new line
  * @n: print from this number
  */
@@ -12,7 +12,7 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 10; i++)
 		{
 			if (i != 98)
 				printf("%d, ", i);
@@ -23,10 +23,10 @@ void print_to_98(int n)
 	{
 		for (j = n; j >= 98; j--)
 		{
-			for (j != 98)
+			if (j != 98)
 				printf("%d, ", j);
 			else if (j == 98)
-				print("%d\n" j);
+				printf("%d\n", j);
 		}
 	}
 }
