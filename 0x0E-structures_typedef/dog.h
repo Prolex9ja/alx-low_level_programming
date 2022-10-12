69acceb26abec542e0097a2550056b9a8a61f634
lex9ja <alexanderedim8@yahoo.com>
2022-10-12 09:49:39 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,14 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - a dog's basic info
+ * struct dog - dog data info
  * @name: First member
  * @age: Second member
  * @owner: Third member
  *
- * Description: Longer description
+ * Describtion: Longer description
  */
-
 struct dog
 {
 	char *name;
@@ -17,3 +16,4 @@ struct dog
 	char *owner;
 };
 
+#endif
