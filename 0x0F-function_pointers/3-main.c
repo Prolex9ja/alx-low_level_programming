e23568a9de6b8567b4943c9266dc11a42499c6cd
lex9ja <alexanderedim8@yahoo.com>
2022-10-16 05:35:21 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 
 /**
- * main - program that performs simple operations
+ * main - program that perfroms simple operations
  * @argc: number of arguments
  * @argv: array of arguments
  *
